a5a340ab656c654a321053ed4f38acaa1d1a646f
FabioLuizz <fabioluis19341@gmail.com>
2026-07-07 18:36:59 -0300

    feat: configuração inicial do Eureka Server e Gateway

---
+++ eureka-server/src/test/java/com/boxflow/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxflow.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
