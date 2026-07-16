48f01372d7cf99412a5912726657562e83a66bce
FabioLuizz <fabioluis19341@gmail.com>
2026-07-16 20:59:39 -0300

    feat: criação do ms-notificacoes

---
+++ ms-notificacoes/src/main/java/com/boxflow/msnotificacoes/MsnotificacoesApplication.java
@@ -0,0 +1,13 @@
+package com.boxflow.msnotificacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnotificacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnotificacoesApplication.class, args);
+	}
+
+}
---
+++ ms-notificacoes/src/test/java/com/boxflow/msnotificacoes/MsnotificacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxflow.msnotificacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnotificacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
